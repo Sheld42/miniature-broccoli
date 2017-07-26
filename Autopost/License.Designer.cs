23f85fe8dcef7be2f4ecec917d73b4228c81edc0
sterhnet <yur-platono@mail.ru>
2017-07-27 02:01:35 +0700

    THE_MOST_EPIC_COMMIT

--- Autopost/License.Designer.cs
+++ Autopost/License.Designer.cs
@@ -96,6 +96,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "License";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "License";
