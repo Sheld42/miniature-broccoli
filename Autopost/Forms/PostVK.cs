67bfd90d83c55c87ec4f9ad2032b07890b85bd9a
sterhnet <yur-platono@mail.ru>
2017-07-27 22:26:09 +0700

    MAKE_MEGA_DEMO

--- Autopost/Forms/PostVK.cs
+++ Autopost/Forms/PostVK.cs
@@ -106,46 +106,15 @@ namespace Autopost
         }
         private void button1_Click(object sender, EventArgs e)      //Начать рассылку
         {
-            kek rt = new kek();
-            try
-            {
-                int a = 5 * 5492;
-                MessageBox.Show((a / (2620 - 1310 * 2)).ToString());
-            }
-            catch
-            {
-                Stopwatch time = new Stopwatch();
-                time.Reset();
-                time.Start();
-                if (rt.regmem())
-                {
-                    time.Stop();
-                    if (5000 < time.ElapsedMilliseconds)
-                    {
-                        PostOndn newForm = new PostOndn();
-                        this.Hide();
-                        newForm.ShowDialog();
-                        this.Close();
-                    }
-                    else
-                    {
-                        Post p = new Post();
-                        p.Text = Textpost;
-                        p.Title = Title;
-                        p.Picturl = Picturl;
-                        p.Pictpath = Pictpath;
-                        Prosmotr newForm = new Prosmotr(p, 1);
-                        newForm.ShowDialog();
-                    }
-                }
-                else
-                {
-                    PostOndn newForm = new PostOndn();
-                    this.Hide();
-                    newForm.ShowDialog();
-                    this.Close();
-                }
-            }
+
+            Post p = new Post();
+            p.Text = Textpost;
+            p.Title = Title;
+            p.Picturl = Picturl;
+            p.Pictpath = Pictpath;
+            Prosmotr newForm = new Prosmotr(p, 1);
+            newForm.ShowDialog();
+
         }
         private void textBox1_TextChanged(object sender, EventArgs e)   //title
         {
